6517c6e60d16507fbd88f8d4ae46e7d57f87fc38
greenPrincess <shanshantina@gmail.com>
2014-01-02 18:14:00 +0800

    message-mult-lan

--- app/messages/en/index.php
+++ app/messages/en/index.php
@@ -1,5 +1,19 @@
 <?php
 $messages = array(
-    "Header" => "Reset your Seagate Password"
+    "Header" => "Reset your Seagate Password",
+    "title" =>"Reset Your Seagate Password",
+    "enterEmail" =>"Enter your email address to reset your password",
+    "checkFolder" =>"You may need to check your spam folder or unblock no-reply@seagate.com.",
+    "submit" =>"Submit",
+    "copyright" =>"Seagate@Copyright 2013",
+    "sentMail" =>"An e-mail has been sent to {example@gmail.com} with further instructions.",
+    "enterPwd" =>"Enter new password",
+    "confirmPwd" =>"Confirm new password",
+    "savedPwd" =>"Your new password has been saved",
+    "usePwd" =>"Please go to Seagate Dashboard and use your new password to login to your Seagate account.",
+    "length" =>"Please enter 6 to 64 characters for your password.",
+    "match" =>"The passwords do not match.",
+    "email" =>"Please enter a correct email.",
+    "exist" =>"The account of {example} does not exist."
 );
 
